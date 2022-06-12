ab76ae8c94d9c0c3cc06127a47fdec6785a05770
Ramazan2002 <ramazanui2002@gmail.com>
2022-06-12 23:03:04 +0300

    Add validation for email field. Handle set_password for user in create action

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Note
+from .validators import validate_email as email_validator
 
 
 class NoteSerializer(serializers.ModelSerializer):
@@ -13,7 +14,20 @@ class NoteSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     notes = NoteSerializer(many=True, read_only=True)
+    email = serializers.EmailField(min_length=10, max_length=100, required=True, validators=[email_validator])
+    password = serializers.CharField(max_length=100, write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password', instance.password)
+            instance.set_password(password)
+        return super().update(instance, validated_data)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'notes']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'notes', 'password']

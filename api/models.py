d1a3c2b5d25d3bce35a7113c942ccbdbd67b04bc
Ramazan2002 <ramazanui2002@gmail.com>
2022-06-13 15:46:49 +0300

    Create Theme model

--- api/models.py
+++ api/models.py
@@ -3,12 +3,21 @@ from django.urls import reverse
 from pytils.translit import slugify
 
 
+class Theme(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey('auth.User', related_name='themes', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Note(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(max_length=10000)
     owner = models.ForeignKey('auth.User', related_name='notes', on_delete=models.CASCADE)
     slug = models.SlugField(max_length=120, unique=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    themes = models.ManyToManyField(Theme, related_name='notes_list')
 
     def __str__(self):
         return self.title
@@ -18,7 +27,8 @@ class Note(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            new_slug = slugify(self.title)
+            self.slug = new_slug
         return super().save(*args, **kwargs)
 
     class Meta:
